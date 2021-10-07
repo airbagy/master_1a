577ce30e5fd04d7012a6869068023ff80262f9a1
airbagy <duanyihan2008@sina.com>
2021-10-07 10:48:00 -0400

    First commit

---
+++ CSC2222H/openmp_playground/helloopenmp.c
@@ -0,0 +1,7 @@
+#include <omp.h>
+#include <stdio.h>
+int main() {
+    omp_set_num_threads(4);
+    #pragma omp parallel
+    printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+}
